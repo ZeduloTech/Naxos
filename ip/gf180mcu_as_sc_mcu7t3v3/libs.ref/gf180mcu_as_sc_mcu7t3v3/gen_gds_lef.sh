984074aa690791311ab6e935421fda600de517b6
snapaa257 <snapaa@zedulo.com>
2026-05-27 21:47:50 +0000

    fix: cell models fixed

--- ip/gf180mcu_as_sc_mcu7t3v3/libs.ref/gf180mcu_as_sc_mcu7t3v3/gen_gds_lef.sh
+++ ip/gf180mcu_as_sc_mcu7t3v3/libs.ref/gf180mcu_as_sc_mcu7t3v3/gen_gds_lef.sh
@@ -11,11 +11,12 @@ sed -i '/string GDS_FILE.*/d' $filename;
 done
 
 for filename in temp/*; do
-echo "calma;lef write;extract all;ext2spice lvs;ext2spice cthresh 100000;ext2spice extresist off;ext2spice;" | magic -dnull -noconsole $filename;
+echo "calma;lef write;extract all;ext2spice lvs;ext2spice cthresh 100000;ext2spice extresist off;ext2spice;" | magic -rcfile automated.magicrc -dnull -noconsole $filename;
 done
-echo "calma;lef write -pinonly;" | magic -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xnor2_2.mag";
-echo "calma;lef write -pinonly;" | magic -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xnor2_4.mag";
-rm -rf temp/
+echo "calma;lef write -pinonly;" | magic -rcfile automated.magicrc -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xnor2_2.mag";
+echo "calma;lef write -pinonly;" | magic -rcfile automated.magicrc -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xnor2_4.mag";
+echo "calma;lef write -pinonly;" | magic -rcfile automated.magicrc -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xor2_2.mag";
+echo "calma;lef write -pinonly;" | magic -rcfile automated.magicrc -dnull -noconsole "temp/gf180mcu_as_sc_mcu7t3v3__xor2_4.mag";
 
 rm -rf gds/
 mkdir gds/
@@ -44,5 +45,20 @@ sed -i '/^M/s/^/M_/' cdl/gf180mcu_as_sc_mcu7t3v3.cdl
 sed -i '/^M/s/^/X_/' spice/gf180mcu_as_sc_mcu7t3v3.spice
 rm -f *.ext
 rm -f *.spice
+
+#cd temp/
+#rm -f *.spice
+#for filename in *.mag; do
+#echo "extract all;ext2sim labels on;ext2sim;extresist tolerance 10;extresist;ext2spice lvs;ext2spice cthresh 0;ext2spice extresist on;ext2spice;" | magic -rcfile ../automated.magicrc -dnull -noconsole $filename;
+#done
+#for filename in *.spice; do
+#cat $filename >> ../spice/gf180mcu_as_sc_mcu7t3v3_parasitics.spice;
+#done
+#cd ..
+rm -f *.ext
+rm -f *.spice
+rm -f *.nodes
+rm -f *.sim
+rm -rf temp/
 
 python3 gen_merged_gds.py gds/*
--- ip/gf180mcu_as_sc_mcu7t3v3/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
+++ ip/gf180mcu_as_sc_mcu7t3v3/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
@@ -49,16 +49,17 @@ reg state;
 wire sr;
 
 assign sr = ~(RN & SN);
+assign Q = state;
 
 always @(posedge CLK or posedge sr) begin
 	if (sr == 1'b1) begin
 	if (RN == 1'b0) begin
-		Q <= 1'b0;
+		state <= 1'b0;
 	end else if (SN == 1'b0) begin
-		Q <= 1'b1;
+		state <= 1'b1;
 	end
 	end else begin
-		Q <= state;
+		state <= D;
 	end
 end
 
@@ -120,6 +121,20 @@ assign Y = A;
 
 endmodule
 
+module gf180mcu_as_sc_mcu7t3v3__buff_16(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+	
+	input A,
+	output Y
+);
+
+assign Y = A;
+
+endmodule
+
 module gf180mcu_as_sc_mcu7t3v3__clkbuff_4(
 	input VPW,
 	input VNW,
@@ -149,6 +164,34 @@ assign Y = A;
 endmodule
 
 module gf180mcu_as_sc_mcu7t3v3__clkbuff_12(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+	
+	input A,
+	output Y
+);
+
+assign Y = A;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__dlybuff_2(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+	
+	input A,
+	output Y
+);
+
+assign Y = A;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__dlybuff_4(
 	input VPW,
 	input VNW,
 	input VDD,
@@ -478,6 +521,36 @@ assign Y = A == B;
 
 endmodule
 
+module gf180mcu_as_sc_mcu7t3v3__xor2_2(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	output Y
+);
+
+assign Y = A != B;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__xor2_4(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	output Y
+);
+
+assign Y = A != B;
+
+endmodule
+
 module gf180mcu_as_sc_mcu7t3v3__maj3_2(
 	input VPW,
 	input VNW,
@@ -523,6 +596,40 @@ module gf180mcu_as_sc_mcu7t3v3__aoi21_2 (
 );
 
 assign Y = ~((A & B) | C);
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__aoi211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A & B) | C | D);
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__aoi211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A & B) | C | D);
 
 endmodule
 
@@ -587,6 +694,40 @@ module gf180mcu_as_sc_mcu7t3v3__ao21_2 (
 );
 
 assign Y = (A & B) | C;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = (A & B) | C | D;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = (A & B) | C | D;
 
 endmodule
 
@@ -774,6 +915,24 @@ assign Y = (A & B & C) | D;
 
 endmodule
 
+
+module gf180mcu_as_sc_mcu7t3v3__oai211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A | B) & C & D);
+
+endmodule
+
 module gf180mcu_as_sc_mcu7t3v3__mux2_2(
 	input VPW,
 	input VNW,
@@ -911,6 +1070,17 @@ module gf180mcu_as_sc_mcu7t3v3__diode_2(
 	input VSS,
 
 	input DIODE
+);
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__hcf_7(
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input HCF
 );
 
 endmodule
